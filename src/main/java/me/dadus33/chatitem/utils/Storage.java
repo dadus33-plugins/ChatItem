3efe63bb49a22090b6c3a0926e1b4f1113ffe61f
Elikill58 <arpetzouille@gmail.com>
2021-11-25 15:30:36 +0100

    Add default values for config

--- src/main/java/me/dadus33/chatitem/utils/Storage.java
+++ src/main/java/me/dadus33/chatitem/utils/Storage.java
@@ -18,27 +18,13 @@ public class Storage {
 
     private FileConfiguration conf;
     public final HashMap<String, HashMap<Short, String>> TRANSLATIONS = new HashMap<>();
-    public final Boolean COLOR_IF_ALREADY_COLORED;
-    public final Boolean FORCE_ADD_AMOUNT;
-    public final Boolean LET_MESSAGE_THROUGH;
-    public final Boolean DENY_IF_NO_ITEM;
-    public final Boolean HAND_DISABLED;
-    public final Boolean SHOW_NO_PERM_NORMAL;
-    public final Boolean CHECK_UPDATE;
-    public final String HAND_NAME;
-    public final String NAME_FORMAT;
-    public final String AMOUNT_FORMAT;
-    public final String NO_PERMISSION_MESSAGE;
-    public final String DENY_MESSAGE;
-    public final String RELOAD_MESSAGE;
-    public final String COOLDOWN_MESSAGE;
-    public final String SECONDS;
-    public final String MINUTES;
-    public final String HOURS;
-    public final String JOIN_UPDATE_MESSAGE;
-    public final String JOIN_UPDATE_HOVER;
-    private final Integer CONFIG_VERSION;
-    public final Long COOLDOWN;
+    public final boolean COLOR_IF_ALREADY_COLORED, FORCE_ADD_AMOUNT, LET_MESSAGE_THROUGH, DENY_IF_NO_ITEM, HAND_DISABLED, SHOW_NO_PERM_NORMAL, CHECK_UPDATE;
+    public final String HAND_NAME, NAME_FORMAT, AMOUNT_FORMAT;
+    public final String NO_PERMISSION_MESSAGE, DENY_MESSAGE, RELOAD_MESSAGE, COOLDOWN_MESSAGE;
+    public final String SECONDS, MINUTES, HOURS;
+    public final String JOIN_UPDATE_MESSAGE, JOIN_UPDATE_HOVER;
+    private final int CONFIG_VERSION;
+    public final long COOLDOWN;
     public final List<Command> ALLOWED_PLUGIN_COMMANDS = new ArrayList<>();
     public final List<String> ALLOWED_DEFAULT_COMMANDS = new ArrayList<>();
     public final List<String> PLACEHOLDERS;
@@ -46,7 +32,7 @@ public class Storage {
 
     public Storage(FileConfiguration cnf) {
         this.conf = cnf;
-        CONFIG_VERSION = conf.getInt("config-version");
+        CONFIG_VERSION = conf.getInt("config-version", 12);
         checkConfigVersion();
         Set<String> keys = conf.getConfigurationSection("Translations").getKeys(false);
         for (String key : keys) {
@@ -59,36 +45,35 @@ public class Storage {
 
             TRANSLATIONS.put(key, entry);
         }
-        COOLDOWN = conf.getLong("General.cooldown");
-        List<String> added = conf.getStringList("General.placeholders");
-        PLACEHOLDERS = ImmutableList.copyOf(added);
-        NAME_FORMAT = color(conf.getString("General.name-format"));
-        AMOUNT_FORMAT = color(conf.getString("General.amount-format"));
-        COLOR_IF_ALREADY_COLORED = conf.getBoolean("General.color-if-already-colored");
-        LET_MESSAGE_THROUGH = conf.getBoolean("General.let-message-through");
-        FORCE_ADD_AMOUNT = conf.getBoolean("General.force-add-amount");
-        DENY_IF_NO_ITEM = conf.getBoolean("General.deny-if-no-item");
-        HAND_DISABLED = conf.getBoolean("General.hand.disabled");
-        SHOW_NO_PERM_NORMAL = conf.getBoolean("General.show-no-permission-message.normal");
-        DENY_MESSAGE = color(conf.getString("Messages.deny-message"));
-        HAND_NAME = color(conf.getString("General.hand.name"));
-        CHECK_UPDATE = conf.getBoolean("General.check-update");
-        RELOAD_MESSAGE = color(conf.getString("Messages.reload-success"));
-        NO_PERMISSION_MESSAGE = color(conf.getString("Messages.no-permission"));
-        COOLDOWN_MESSAGE = color(conf.getString("Messages.cooldown-message"));
-        JOIN_UPDATE_MESSAGE = color(conf.getString("Messages.join-update.message"));
-        JOIN_UPDATE_HOVER = color(conf.getString("Messages.join-update.hover"));
-        
-        SECONDS = color(conf.getString("Messages.seconds"));
-        MINUTES = color(conf.getString("Messages.minutes"));
-        HOURS = color(conf.getString("Messages.hours"));
+        PLACEHOLDERS = ImmutableList.copyOf(conf.getStringList("General.placeholders"));
+        NAME_FORMAT = color(conf.getString("General.name-format", "&b&l&o{name} {amount}&r"));
+        AMOUNT_FORMAT = color(conf.getString("General.amount-format", "x{times}"));
+        COLOR_IF_ALREADY_COLORED = conf.getBoolean("General.color-if-already-colored", true);
+        LET_MESSAGE_THROUGH = conf.getBoolean("General.let-message-through", true);
+        FORCE_ADD_AMOUNT = conf.getBoolean("General.force-add-amount", true);
+        DENY_IF_NO_ITEM = conf.getBoolean("General.deny-if-no-item", false);
+        COOLDOWN = conf.getLong("General.cooldown", 60);
+        SHOW_NO_PERM_NORMAL = conf.getBoolean("General.show-no-permission-message.normal", true);
+        HAND_DISABLED = conf.getBoolean("General.hand.disabled", false);
+        HAND_NAME = color(conf.getString("General.hand.name", "&b&l&o{display-name}&b&l&o's hand"));
         HAND_TOOLTIP = conf.getStringList("General.hand.tooltip");
+        CHECK_UPDATE = conf.getBoolean("General.check-update", true);
+        DENY_MESSAGE = color(conf.getString("Messages.deny-message", "&c&lYou have no item in hand!"));
+        RELOAD_MESSAGE = color(conf.getString("Messages.reload-success", "&b&lSuccessful reload!"));
+        NO_PERMISSION_MESSAGE = color(conf.getString("Messages.no-permission", "&c&lI'm sorry, but you are not allowed to use the placeholder in chat!"));
+        COOLDOWN_MESSAGE = color(conf.getString("Messages.cooldown-message", "&c&lYou can only use items in chat once a minute! You have {remaining} left!"));
+        JOIN_UPDATE_MESSAGE = color(conf.getString("Messages.join-update.message", "&cA new version of ChatItem is available. &aClick here to download."));
+        JOIN_UPDATE_HOVER = color(conf.getString("Messages.join-update.hover", "&6Click to go to spigot page !"));
+        
+        SECONDS = color(conf.getString("Messages.seconds", " seconds"));
+        MINUTES = color(conf.getString("Messages.minutes", " minutes"));
+        HOURS = color(conf.getString("Messages.hours", " hours"));
         colorStringList(HAND_TOOLTIP);
         final List<String> cmds = conf.getStringList("General.commands");
         Bukkit.getScheduler().runTaskLaterAsynchronously(ChatItem.getInstance(), () -> {
             for(String s : cmds){
                 Command c = Bukkit.getPluginCommand(s);
-                if(c!=null) {
+                if(c != null) {
                     ALLOWED_PLUGIN_COMMANDS.add(c);
                 } else {
                     ALLOWED_DEFAULT_COMMANDS.add(s);
