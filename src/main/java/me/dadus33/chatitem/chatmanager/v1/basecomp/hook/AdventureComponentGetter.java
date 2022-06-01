848a44c5dacd244adf52a51c9fde676d696f4cd1
Elikill58 <arpetzouille@mail.com>
2022-06-01 19:01:06 +0200

    Finally fix with Kyori adventure messae

--- src/main/java/me/dadus33/chatitem/chatmanager/v1/basecomp/hook/AdventureComponentGetter.java
+++ src/main/java/me/dadus33/chatitem/chatmanager/v1/basecomp/hook/AdventureComponentGetter.java
@@ -2,6 +2,12 @@ package me.dadus33.chatitem.chatmanager.v1.basecomp.hook;
 
 import java.util.Arrays;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import me.dadus33.chatitem.ChatItem;
 import me.dadus33.chatitem.chatmanager.v1.basecomp.IBaseComponentGetter;
 import me.dadus33.chatitem.chatmanager.v1.packets.ChatItemPacket;
 import me.dadus33.chatitem.utils.PacketUtils;
@@ -31,11 +37,64 @@ public class AdventureComponentGetter implements IBaseComponentGetter {
 	@Override
 	public String getBaseComponentAsJSON(ChatItemPacket packet) {
 		Component comp = packet.getContent().getSpecificModifier(Component.class).readSafely(0);
-		return comp == null ? null : ComponentSerializer.toString(BungeeComponentSerializer.legacy().serialize(comp));
+		if(comp == null)
+			return null;
+		String json = ComponentSerializer.toString(BungeeComponentSerializer.legacy().serialize(comp).clone());
+		JsonObject jsonObj = JsonParser.parseString(json).getAsJsonObject();
+		JsonObject next = new JsonObject();
+		next.add("extra", jsonObj.get("with"));
+		return next.toString();
 	}
 
 	@Override
 	public void writeJson(ChatItemPacket packet, String json) {
-		packet.getContent().getSpecificModifier(Component.class).write(0, BungeeComponentSerializer.get().deserialize(ComponentSerializer.parse(json)));
+		JsonObject next = new JsonObject();
+		next.addProperty("translate", "chat.type.text");
+		next.add("with", fixParsedArray(JsonParser.parseString(json).getAsJsonObject().getAsJsonArray("extra")));
+		ChatItem.debug("Adventure Json: " + next.toString());
+		packet.getContent().getSpecificModifier(Component.class).write(0, BungeeComponentSerializer.legacy().deserialize(ComponentSerializer.parse(next.toString())));
+	}
+	
+	private JsonArray fixParsedArray(JsonArray arr) {
+		JsonArray next = new JsonArray();
+		for(JsonElement e : arr) {
+			if(e.isJsonArray()) {
+				JsonObject obj = null;
+				JsonArray extra = new JsonArray();
+				for(JsonElement element : e.getAsJsonArray()) {
+					JsonElement fixed = checkElement(element);
+					if(obj == null && fixed.isJsonObject()) {
+						obj = fixed.getAsJsonObject();
+					} else {
+						extra.add(fixed);
+					}
+				}
+				if(obj != null && extra.size() > 0) {
+					obj.add("extra", extra);
+				}
+				next.add(obj);
+			} else
+				next.add(checkElement(e));
+		}
+		return next;
+	}
+	
+	private JsonElement checkElement(JsonElement e) {
+		if(e.isJsonObject()) {
+			JsonObject o = e.getAsJsonObject();
+			if(o.has("hoverEvent")) {// only hover event
+				JsonObject hoverObject = o.getAsJsonObject("hoverEvent");
+				if(hoverObject.get("action").getAsString().equalsIgnoreCase("show_item") && hoverObject.has("value")) {// if it's item to fix
+					String val = hoverObject.get("value").getAsString();
+					// now, we should fix the value
+					val = val.replaceAll("\\\"", "\"");
+					ChatItem.debug("Fixed json: " + ("{" + val.substring(1, val.length() - 1) + "}"));
+					hoverObject.remove("value");
+					hoverObject.add("contents", JsonParser.parseString("{" + val.substring(1, val.length() - 1) + "}"));
+					o.add("hoverEvent", hoverObject);
+				}
+			}
+		}
+		return e;
 	}
 }
--- src/main/java/me/dadus33/chatitem/commands/ChatItemCommand.java
+++ src/main/java/me/dadus33/chatitem/commands/ChatItemCommand.java
@@ -23,47 +23,49 @@ import net.md_5.bungee.api.chat.TextComponent;
 
 public class ChatItemCommand implements CommandExecutor, TabExecutor {
 
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	@Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    	if(!(sender instanceof Player)) {
-    		Messages.sendMessage(sender, "only-players");
-    		return false;
-    	}
-    	Player p = (Player) sender;
-    	if(args.length == 0) {
-    		Messages.sendMessage(p, "chatitem-cmd.help");
-    	} else if(args[0].equalsIgnoreCase("admin")) {
-    		InventoryListener.open(p);
-    	} else if(args[0].equalsIgnoreCase("reload")) {
-    		ChatItem.reload(sender);
-    	} else if(args[0].equalsIgnoreCase("link") || args[0].equalsIgnoreCase("links")) {
-    		ConfigurationSection config = ChatItem.getInstance().getConfig().getConfigurationSection("messages.chatitem-cmd.links");
-    		TextComponent text = new TextComponent(Storage.color(config.getString("begin")));
-    		for(String key : config.getConfigurationSection("list").getKeys(false)) {
-    			ConfigurationSection linkConfig = config.getConfigurationSection("list." + key);
-        		TextComponent linkComp = new TextComponent(Storage.color(linkConfig.getString("message")));
-        		String hover = Storage.color(linkConfig.getString("hover"));
-        		if(hover != null)
-        			linkComp.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hover).create()));
-        		String link = linkConfig.getString("link"); 
-        		if(link != null)
-        			linkComp.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
-        		text.addExtra(linkComp);
-    		}
-    		p.spigot().sendMessage(text);
-    	} else {
-    		Messages.sendMessage(p, "chatitem-cmd.help");
-    	}
-        return false;
-    }
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			Messages.sendMessage(sender, "only-players");
+			return false;
+		}
+		Player p = (Player) sender;
+		if (args.length == 0) {
+			Messages.sendMessage(p, "chatitem-cmd.help");
+		} else if (args[0].equalsIgnoreCase("admin")) {
+			InventoryListener.open(p);
+		} else if (args[0].equalsIgnoreCase("reload")) {
+			ChatItem.reload(sender);
+		} else if (args[0].equalsIgnoreCase("link") || args[0].equalsIgnoreCase("links")) {
+			ConfigurationSection config = ChatItem.getInstance().getConfig()
+					.getConfigurationSection("messages.chatitem-cmd.links");
+			TextComponent text = new TextComponent(Storage.color(config.getString("begin")));
+			for (String key : config.getConfigurationSection("list").getKeys(false)) {
+				ConfigurationSection linkConfig = config.getConfigurationSection("list." + key);
+				TextComponent linkComp = new TextComponent(Storage.color(linkConfig.getString("message")));
+				String hover = Storage.color(linkConfig.getString("hover"));
+				if (hover != null)
+					linkComp.setHoverEvent(
+							new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(hover).create()));
+				String link = linkConfig.getString("link");
+				if (link != null)
+					linkComp.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
+				text.addExtra(linkComp);
+			}
+			p.spigot().sendMessage(text);
+		} else {
+			Messages.sendMessage(p, "chatitem-cmd.help");
+		}
+		return false;
+	}
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] arg) {
 		List<String> list = new ArrayList<>();
 		String prefix = arg[arg.length - 1].toLowerCase(Locale.ROOT);
-		for(String s : Arrays.asList("help", "admin", "reload"))
-			if(prefix.isEmpty() || s.startsWith(prefix))
+		for (String s : Arrays.asList("help", "admin", "reload", "link"))
+			if (prefix.isEmpty() || s.startsWith(prefix))
 				list.add(s);
 		return list.isEmpty() ? null : list;
 	}
