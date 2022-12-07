26b0837784066a7a9e6f8250d2b2d046b05af2dc
Elikill58 <arpetzouille@gmail.com>
2022-12-07 15:58:18 +0100

    Temporary remove EcoEnchants support to prevent error

--- src/main/java/me/dadus33/chatitem/hook/EcoEnchantsSupport.java
+++ src/main/java/me/dadus33/chatitem/hook/EcoEnchantsSupport.java
@@ -5,16 +5,14 @@ import java.util.List;
 
 import org.bukkit.inventory.ItemStack;
 
-import com.willfp.ecoenchants.display.EnchantmentCache;
-import com.willfp.ecoenchants.enchantments.util.EnchantChecks;
-
 public class EcoEnchantsSupport {
 
 	public static List<String> getLores(ItemStack item) {
 		List<String> lores = new ArrayList<>();
+		/*com.willfp.ecoenchants.
 		EnchantChecks.getEnchantsOnItem(item).forEach((en, lvl) -> {
 			lores.add(EnchantmentCache.getEntry(en).getNameWithLevel(lvl));
-		});
+		});*/
 		return lores;
 	}
 	
